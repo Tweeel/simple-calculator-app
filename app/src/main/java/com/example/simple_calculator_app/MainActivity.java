5e536db5a7c2976b45661dcc5eb4b0205b042273
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-19 14:50:14 +0100

    declaration ds fonction

--- app/src/main/java/com/example/simple_calculator_app/MainActivity.java
+++ app/src/main/java/com/example/simple_calculator_app/MainActivity.java
@@ -13,4 +13,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void fun_0(View view) {
+    }
+
+    public void fun_1(View view) {
+    }
+
+    public void fun_2(View view) {
+    }
+
+    public void fun_3(View view) {
+    }
+
+    public void fun_4(View view) {
+    }
+
+    public void fun_5(View view) {
+    }
+
+    public void fun_6(View view) {
+    }
+
+    public void fun_7(View view) {
+    }
+
+    public void fun_8(View view) {
+    }
+
+    public void fun_9(View view) {
+    }
+
+    public void plus_fun(View view) {
+    }
+
+    public void minus_fun(View view) {
+    }
+
+    public void multiplication_fun(View view) {
+    }
+
+    public void divide_fun(View view) {
+    }
+
+    public void equal_fun(View view) {
+    }
+
+    public void dot_fun(View view) {
+    }
+
+    public void start_bracket_fun(View view) {
+    }
+
+    public void end_bracket_fun(View view) {
+    }
+
+    public void ac_fun(View view) {
+    }
 }
